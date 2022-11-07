344e76ab7b6a944a8edd2ae84073bfc4da4075b8
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-07 18:09:46 -0500

    finished challenge1

--- game.py
+++ game.py
@@ -11,6 +11,12 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+FUSCHIA = (255, 0, 255)
+GRAY = (100, 100, 100)
+ORANGE = (250, 141, 7)
+LIGHTGREEN = (25, 184, 22)
 
 # game loop
 running = True
@@ -23,8 +29,15 @@ while running:
     # clear the screen
     screen.fill((WHITE))
     #draw a circle
-    color = (255, 0, 255) #rgb values
-    position = (250, 250) # x, y position
-    pygame.draw.circle(screen, color, position, 75) #75 is the radius
+    position1 = (250, 250) # x, y position
+    pygame.draw.circle(screen, YELLOW, position1, 50) #75 is the radius
+    position2 =  (50,50)
+    pygame.draw.circle(screen, RED, position2, 50) #75 is the radius
+    position3 =  (450,50)
+    pygame.draw.circle(screen, ORANGE, position3, 50) #75 is the radius
+    position4 =  (50, 450)
+    pygame.draw.circle(screen, LIGHTGREEN, position4, 50) #75 is the radius
+    position5 =  (450, 450)
+    pygame.draw.circle(screen, CYAN, position5, 50) #75 is the radius
     #update display
     pygame.display.flip()
