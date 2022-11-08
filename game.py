c2d1f5c223897c2ada23efe8a96dc63ff2677d0f
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-07 19:42:15 -0500

    added objects to game

--- game.py
+++ game.py
@@ -26,19 +26,17 @@ class GameObject(pygame.sprite.Sprite):
     Defines four parameters: x, y, width, and height.
     It stores three attributes: self.surf, self.x, and self.y.
     """
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x_position, y_position, image):
         super(GameObject, self).__init__()
-        self.surf = pygame.Surface((width, height))
-        self.surf.fill ((color))
-        self.rect = self.surf.get_rect()
-        self.x = x
-        self.y = y
+        self.surf = pygame.image.load(image) # ADD!
+        self.x_position = x_position
+        self.y_position = y_position
 
     def render (self, screen):
         """
         This method is responsible for drawing the GameObject's surface to the screen.
         """
-        screen.blit(self.surf, (self.x, self.y))
+        screen.blit(self.surf, (self.x_position, self.y_position))
 
 # game loop
 running = True
@@ -49,11 +47,15 @@ while running:
             running = False
 
     # Make an instance of GameObject
-    box = GameObject(120, 300, 50, 50, FUSCHIA)
-    box2 = GameObject(90, 40, 50, 50, BLUE )
+    apple = GameObject(200, 300, 'images/apple.png')
+    strawberry= GameObject(400, 70, 'images/strawberry.png')
+    mushroom = GameObject(300, 150,'images/mushroom.jpeg')
     screen.fill (WHITE)
-    box.render(screen)
-    box2.render(screen)
+    apple.render(screen)
+    strawberry.render(screen)
+    mushroom.render(screen)
+    
+    
     #update display
     pygame.display.flip()
 
